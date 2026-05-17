c4d776eab24da4530f828cb215b3a8ed9cbd9908
replalabs <engineering@replalabs.invalid>
2026-05-17 19:14:00 +0900

    wire tracing-subscriber and log emitted deltas in the binary

--- src/bin/sequencer.rs
+++ src/bin/sequencer.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
 use repla::runtime::{SequencerConfig, SequencerRuntime};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "REPLA sequencer node")]
@@ -14,13 +16,21 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")))
+        .init();
     let args = Args::parse();
     let cfg = SequencerConfig {
         l3_id: [0u8; 32],
         slot_time_ms: args.slot_time_ms,
         settle_interval_slots: args.settle_interval,
     };
+    info!("repla-sequencer starting (open-source build)");
     let rt = SequencerRuntime::new(cfg);
-    rt.run(|_delta| async move { Ok(()) }).await?;
+    rt.run(|delta| async move {
+        info!(?delta, "delta emitted");
+        Ok(())
+    })
+    .await?;
     Ok(())
 }
